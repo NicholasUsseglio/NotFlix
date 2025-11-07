5ec38ea5888d9b022942a0a75a819c27c56e78ef
NicholasUsseglio <jackwrite55595@gmail.com>
2025-11-07 15:19:28 +0100

    Spostato progetto su repo pubblica

---
+++ Notflix/springVideo/src/main/java/com/notflix/springVideo/SpringVideoApplication.java
@@ -0,0 +1,13 @@
+package com.notflix.springVideo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringVideoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringVideoApplication.class, args);
+	}
+
+}
---
+++ Notflix/springVideo/src/test/java/com/notflix/springVideo/SpringVideoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.notflix.springVideo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringVideoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
